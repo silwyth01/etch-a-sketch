47d648ef690e95bf53155e7ba1af006b1679ea7c
silwyth01 <silwyth01@gmail.com>
2021-07-18 20:44:08 -0400

    color fuyo

--- scripts.js
+++ scripts.js
@@ -8,13 +8,21 @@ function setRow(column) {
         let square = document.createElement('div');
         square.classList.add('square-style');
         square.style.backgroundColor = "white";
-        square.style.width = `${((500 / side) - 2).toString()}px`;
-        square.style.height = `${((500 / side) - 2).toString()}px`;
+        square.style.width = `${((500 / side) - 2)}px`;
+        square.style.height = `${((500 / side) - 2)}px`;
 
         column.appendChild(square);
         square.addEventListener("mouseover", function (event) {
-            event.target.style.backgroundColor = "black";
-        })
+            var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            console.log(event.target.style.backgroundColor);
+            // let darkness = event.target.style.filter`brightness(0.9)`;
+            if (event.target.style.backgroundColor.toString() === 'white') {
+                event.target.style.backgroundColor = randomColor;
+            } else {
+                event.target.style.filter += `brightness(0.9)`;
+            }
+        }
+        )
     }
 
 }
